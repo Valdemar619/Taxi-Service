e52f5a9b2081b54f96c3bacec621b968aca2107b
Володимир <vova.malyarchuk2@gmail.com>
2023-07-03 19:45:10 +0300

    local

--- src/main/java/taxi/service/service/impl/AuthenticationServiceImpl.java
+++ src/main/java/taxi/service/service/impl/AuthenticationServiceImpl.java
@@ -1,9 +1,11 @@
-package taxi.service;
+package taxi.service.service.impl;
 
 import java.util.Optional;
 import taxi.exception.AuthenticationException;
 import taxi.lib.Inject;
 import taxi.model.Driver;
+import taxi.service.AuthenticationService;
+import taxi.service.DriverService;
 
 public class AuthenticationServiceImpl implements AuthenticationService {
     @Inject
